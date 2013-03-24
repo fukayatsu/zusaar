f1c4f0e378b9a229abea28cb522706f67583f775
fukayatsu <fukayatsu@gmail.com>
2013-03-24 11:59:39 +0900

    Add spec

--- spec/zussar/client_spec.rb
+++ spec/zussar/client_spec.rb
@@ -10,6 +10,15 @@ describe Zussar::Client do
         events.size.should == 10
       end
     end
+    context "search with {event_id: 545003}" do
+      it "returns 1 event" do
+        client = Zussar::Client.new
+        result = client.search_events(event_id: 545003) # http://www.zusaar.com/event/545003
+        events  = result['event']
+        events.size.should == 1
+        events[0]["event_id"] = 545003
+      end
+    end
   end
 
   describe '#user_search' do
