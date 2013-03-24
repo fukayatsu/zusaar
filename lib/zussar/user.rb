a01b4345c0af3b9e905881ece02c31d65247fdb0
fukayatsu <fukayatsu@gmail.com>
2013-03-24 14:20:44 +0900

    zussar apiのバグ対応

--- lib/zussar/user.rb
+++ lib/zussar/user.rb
@@ -3,5 +3,21 @@ module Zussar
     attr_reader :user_id, :profile_url, :nickname, :status
 
     alias_method :id, :user_id
+
+    def initialize(attrs = {})
+      if attrs['profile_url']
+        # zussar apiのバグ対応
+        attrs['profile_url'] = fix_url(attrs['profile_url'])
+      end
+
+      super(attrs)
+    end
+
+  private
+
+    def fix_url(url)
+      return nil if url.nil?
+      url.gsub("http//", "http://").gsub("https//", "https://")
+    end
   end
 end
