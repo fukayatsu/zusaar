c2153c8d9890c9b892c9e7ad19524473e5c02eed
fukayatsu <fukayatsu@gmail.com>
2013-04-05 19:43:17 +0900

    update for zusaar-bug-fix

--- lib/zusaar/user.rb
+++ lib/zusaar/user.rb
@@ -3,21 +3,5 @@ module Zusaar
     attr_reader :user_id, :profile_url, :nickname, :status
 
     alias_method :id, :user_id
-
-    def initialize(attrs = {})
-      if attrs['profile_url']
-        # zusaar apiのバグ対応
-        attrs['profile_url'] = fix_url(attrs['profile_url'])
-      end
-
-      super(attrs)
-    end
-
-  private
-
-    def fix_url(url)
-      return nil if url.nil?
-      url.gsub("http//", "http://").gsub("https//", "https://")
-    end
   end
 end
